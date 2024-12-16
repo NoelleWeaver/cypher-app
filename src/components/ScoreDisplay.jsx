484226de289bc00ec63bb64a8c0789413f65de18
NoelleWeaver <141285591+NoelleWeaver@users.noreply.github.com>
2024-12-16 13:58:20 -0700

    Setup

---
+++ src/components/ScoreDisplay.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ScoreDisplay = () => {
+  return (
+    <div>ScoreDisplay</div>
+  )
+}
+
+export default ScoreDisplay
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import Cypher from './pages/cipher';
+import './styles/index.css';
+import App from './App';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 const router = createBrowserRouter([
-  { path: '/', element: <Cypher /> }
+  { path: '/', element: <App /> }
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
